3d4f4060c023ddcf35b41adf3e7c1a7f95abba6f
David Jelenc <david.jelenc@fri.uni-lj.si>
2024-12-13 06:44:47 +0100

    Initial commit

---
+++ app/src/main/java/ep/rest/BookService.kt
@@ -0,0 +1,54 @@
+package ep.rest
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+object BookService {
+
+    interface RestApi {
+
+        companion object {
+            // AVD emulator
+            // Potrebno je še nastaviti posredovanje števil vrat
+            const val URL = "http://10.0.2.2:8080/netbeans/mvc-rest/api/"
+        }
+
+        @GET("books")
+        fun getAll(): Call<List<Book>>
+
+        @GET("books/{id}")
+        fun get(@Path("id") id: Int): Call<Book>
+
+        @FormUrlEncoded
+        @POST("books")
+        fun insert(
+            @Field("author") author: String,
+            @Field("title") title: String,
+            @Field("price") price: Double,
+            @Field("year") year: Int,
+            @Field("description") description: String
+        ): Call<Void>
+
+        @FormUrlEncoded
+        @PUT("books/{id}")
+        fun update(
+            @Path("id") id: Int,
+            @Field("author") author: String,
+            @Field("title") title: String,
+            @Field("price") price: Double,
+            @Field("year") year: Int,
+            @Field("description") description: String
+        ): Call<Void>
+    }
+
+    val instance: RestApi by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(RestApi.URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(RestApi::class.java)
+    }
+}
